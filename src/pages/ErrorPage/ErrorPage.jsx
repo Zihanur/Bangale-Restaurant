0ed2b43865857df78ee000984e531c9fe958e963
Zihanur Rahman <zihanur15-2188@diu.edu.bd>
2023-05-04 16:45:05 +0600

    Added Final Updated

--- src/pages/ErrorPage/ErrorPage.jsx
+++ src/pages/ErrorPage/ErrorPage.jsx
@@ -3,15 +3,23 @@ import { Container } from "react-bootstrap";
 import { useRouteError } from "react-router-dom";
 
 const ErrorPage = () => {
-  const {error, status} = useRouteError();
+  const { error, status } = useRouteError();
   return (
     <Container className="text-center">
       <h1>Oops!</h1>
-      {
-        error && <img className="fluid" style={{height:"400px", width:"650px"}} src="https://images.pexels.com/photos/1888015/pexels-photo-1888015.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" alt="Error Image" />
-      }
+      {error && (
+        <img
+          className="fluid"
+          style={{ height: "400px", width: "650px" }}
+          src="https://images.pexels.com/photos/1888015/pexels-photo-1888015.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+          alt="Error Image"
+        />
+      )}
       <p className="fs-4 text-danger mt-5">
-        <>{"Error Message: "}{error.statusText || error.message}</>
+        <>
+          {"Error Message: "}
+          {error.statusText || error.message}
+        </>
       </p>
     </Container>
   );
